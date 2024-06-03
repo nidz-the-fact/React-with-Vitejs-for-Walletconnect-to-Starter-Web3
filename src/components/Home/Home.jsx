8a3f1b70c9de1ae2703d79a22ec984636f9174d5
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2024-06-04 06:45:34 +0700

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,10 +5,10 @@ function Home() {
     return (
         <div className="mockup-browser border bg-base-300">
             <div className="mockup-browser-toolbar">
-                <div className="input">https://example.com</div>
+                <div className="input">https://linktr.ee/nid_z</div>
             </div>
             <div className="flex justify-center px-4 py-16 bg-base-200">
-                <pre data-prefix="$"><code> npm i daisyui </code></pre>
+                <pre data-prefix="$"><code> git clone https://github.com/nidz-the-fact/React-with-Vitejs-for-Walletconnect-to-Starter-Web3.git</code></pre>
             </div>
             <ReadContract />
         </div>
